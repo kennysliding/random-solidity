7338a5946ea3dc020f598faa0614aff8fe1191bc
burningalc <kennychiu.v@gmail.com>
2022-02-03 21:04:53 +0800

    remove unwanted files

--- wrapped-token/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var KCoin = artifacts.require("KCoin");
-var WrappedKCoin = artifacts.require("WrappedKCoin");
-
-module.exports = async function (deployer) {
-  await deployer.deploy(KCoin);
-  const kcoin = await KCoin.deployed();
-  await deployer.deploy(WrappedKCoin, kcoin.address);
-};
