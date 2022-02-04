5018770e4621c5c5592aed675c4f6353fa8ef541
burningalc <kennychiu.v@gmail.com>
2022-02-04 11:34:12 +0800

    visibility change

--- wrapped-token/contracts/KCoin.sol
+++ wrapped-token/contracts/KCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KCoin is ERC20 {
-  constructor() public ERC20("KCoin", "KCO") {
+  constructor() ERC20("KCoin", "KCO") {
     _mint(msg.sender, 10000);
   }
 }
