af3ab4355ebaca2cabca97dd21378d9fa9c5d6ba
burningalc <kennychiu.v@gmail.com>
2022-02-04 11:34:10 +0800

    visibility change

--- timelock/contracts/TimeCoin.sol
+++ timelock/contracts/TimeCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TimeCoin is ERC20 {
-  constructor() public ERC20("TimeCoin", "TCO") {
+  constructor() ERC20("TimeCoin", "TCO") {
     _mint(msg.sender, 10000);
   }
 }
