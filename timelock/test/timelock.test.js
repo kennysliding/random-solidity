9ce9c47050de0529212a250d1a98c42bbfe27e73
burningalc <kennychiu.v@gmail.com>
2022-02-04 11:30:34 +0800

    tests reform

--- timelock/test/timelock.test.js
+++ timelock/test/timelock.test.js
@@ -5,55 +5,97 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-contract("Timelocker", (accounts) => {
-  it("should put 10000 timecoin in the first account", async () => {
-    const timeCoin = await TimeCoin.deployed();
-    const balance = await timeCoin.balanceOf(accounts[0]);
-    assert.equal(balance.toNumber(), 10000);
-  });
-
-  it("try release a non-exist locker", async () => {
-    const timelocker = await Timelocker.deployed();
-    try {
-      await timelocker.release(1, { from: accounts[0] });
-      assert.fail();
-    } catch (error) {
-      assert.include(error.message, "Locker does not exist");
-    }
-  });
-
-  it("should deposit 200 timecoin in timelocker from account 0 to account 1", async () => {
-    const timeCoin = await TimeCoin.deployed();
-    const timelocker = await Timelocker.deployed();
-    await timeCoin.approve(timelocker.address, 200, { from: accounts[0] });
-    let balance0 = await timeCoin.balanceOf(accounts[0]);
-    assert.equal(balance0.toNumber(), 10000);
-
-    let blockTime = await timelocker.getCurrentBlockTime();
-    blockTime = blockTime.toNumber() + 5;
-    await timelocker.deposit(accounts[1], blockTime, 200, {
-      from: accounts[0],
+contract("Timelocker & TimeCoin", (accounts) => {
+  describe("Initialization", () => {
+    it("should put 10000 timecoin in the first account", async () => {
+      const timeCoin = await TimeCoin.deployed();
+      const balance = await timeCoin.balanceOf(accounts[0]);
+      assert.equal(balance.toNumber(), 10000);
     });
-    balance0 = await timeCoin.balanceOf(accounts[0]);
-    assert.equal(balance0.toNumber(), 9800);
+  });
 
-    try {
-      await timelocker.release(0, { from: accounts[1] });
-      assert.fail();
-    } catch (error) {
-      assert.include(error.message, "Not yet can release");
-    }
+  describe("Run without error", () => {
+    it("should deposit 200 timecoin in timelocker from account 0 to account 1", async () => {
+      const timeCoin = await TimeCoin.deployed();
+      const timelocker = await Timelocker.deployed();
+      await timeCoin.approve(timelocker.address, 200, { from: accounts[0] });
+      let balance0 = await timeCoin.balanceOf(accounts[0]);
+      assert.equal(balance0.toNumber(), 10000);
 
-    await sleep(8000);
-    await timelocker.release(0, { from: accounts[1] });
-    const balance1 = await timeCoin.balanceOf(accounts[1]);
-    assert.equal(balance1.toNumber(), 200);
+      let blockTime = await timelocker.getCurrentBlockTime();
+      blockTime = blockTime.toNumber() + 5;
+      await timelocker.deposit(accounts[1], blockTime, 200, {
+        from: accounts[0],
+      });
+      balance0 = await timeCoin.balanceOf(accounts[0]);
+      assert.equal(balance0.toNumber(), 9800);
 
-    try {
-      await timelocker.release(0, { from: accounts[1] });
-      assert.fail();
-    } catch (error) {
-      assert.include(error.message, "Already been released");
-    }
+      let lockers = await timelocker.getLockers({ from: accounts[1] });
+      let lockerId = lockers.length - 1;
+      await sleep(8000);
+      await timelocker.release(lockerId, { from: accounts[1] });
+      const balance1 = await timeCoin.balanceOf(accounts[1]);
+      assert.equal(balance1.toNumber(), 200);
+    });
+  });
+
+  describe("Run with error", () => {
+    it("try release a non-exist locker", async () => {
+      const timelocker = await Timelocker.deployed();
+      let lockers = await timelocker.getLockers({ from: accounts[1] });
+      let lockerId = lockers.length - 1;
+      try {
+        await timelocker.release(lockerId + 1, { from: accounts[0] });
+        assert.fail();
+      } catch (error) {
+        assert.include(error.message, "Locker does not exist");
+      }
+    });
+
+    it("try release before locker timestamp", async () => {
+      const timeCoin = await TimeCoin.deployed();
+      const timelocker = await Timelocker.deployed();
+      await timeCoin.approve(timelocker.address, 200, { from: accounts[0] });
+
+      let blockTime = await timelocker.getCurrentBlockTime();
+      blockTime = blockTime.toNumber() + 10;
+      await timelocker.deposit(accounts[1], blockTime, 200, {
+        from: accounts[0],
+      });
+      let lockers = await timelocker.getLockers({ from: accounts[1] });
+      let lockerId = lockers.length - 1;
+
+      try {
+        await timelocker.release(lockerId, { from: accounts[1] });
+        assert.fail();
+      } catch (error) {
+        assert.include(error.message, "Not yet can release");
+      }
+    });
+
+    it("try release a released locker", async () => {
+      const timeCoin = await TimeCoin.deployed();
+      const timelocker = await Timelocker.deployed();
+      await timeCoin.approve(timelocker.address, 200, { from: accounts[0] });
+
+      let blockTime = await timelocker.getCurrentBlockTime();
+      blockTime = blockTime.toNumber() + 5;
+      await timelocker.deposit(accounts[1], blockTime, 200, {
+        from: accounts[0],
+      });
+
+      let lockers = await timelocker.getLockers({ from: accounts[1] });
+      let lockerId = lockers.length - 1;
+      console.debug(lockers);
+      await sleep(8000);
+      await timelocker.release(lockerId, { from: accounts[1] });
+
+      try {
+        await timelocker.release(lockerId, { from: accounts[1] });
+        assert.fail();
+      } catch (error) {
+        assert.include(error.message, "Already been released");
+      }
+    });
   });
 });
